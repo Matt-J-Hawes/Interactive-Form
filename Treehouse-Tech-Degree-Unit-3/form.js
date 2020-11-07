0eccb8e61a3e208b3677c602a491a797f07245e0
Matt Hawes <73285477+Matt-J-Hawes@users.noreply.github.com>
2020-11-07 18:31:40 +0000

    Third Draft Ready - Complete project for review.

--- Treehouse-Tech-Degree-Unit-3/form.js
+++ Treehouse-Tech-Degree-Unit-3/form.js
@@ -108,10 +108,46 @@ for(let x = 0; x < selectColor.length; x++){
 
 const workshops = document.querySelector(".activities")
 const totalCostText = document.createElement("h3")
-const totalCost = 0;
+const checkboxes = workshops.getElementsByTagName("input")
+const label = workshops.getElementsByTagName("label")
+let   totalCost = 0;
+
 totalCostText.innerHTML = `Total Cost: $${totalCost}`
 workshops.appendChild(totalCostText)
 
+workshops.addEventListener("change", function(e){
+let clicked = e.target;
+let clickedType = clicked.getAttribute("data-cost")
+let eventTime = clicked.getAttribute("data-day-and-time")
+const price = parseInt(clickedType)
+
+		if(clicked.checked){
+			totalCost += price
+			totalCostText.innerHTML = `Total Cost: $${totalCost}`}
+
+		if(!clicked.checked){
+			totalCost -= price
+			totalCostText.innerHTML = `Total Cost: $${totalCost}`
+		}
+
+for(let i = 0; i < checkboxes.length; i++){
+for(let j = 0; j < label.length; i++){
+let date = checkboxes[i].getAttribute("data-day-and-time")
+		if(clicked.checked && date === eventTime){
+		if(checkboxes[i] !== clicked){
+			checkboxes[i].setAttribute('disabled', true)
+			label[i].style.color = "grey"
+			label[i].style.textDecoration = "line-through"
+		}}
+
+	    if(!clicked.checked){
+			checkboxes[i].removeAttribute('disabled')
+			label[i].style.color = "inherit"
+			label[i].style.textDecoration = "none"
+
+		}
+	}}
+})
 
 
 /*************** PAYMENT SECTION ******************/
@@ -140,7 +176,7 @@ paymentSelection.addEventListener("change", function(){
 			creditCardDiv.style.display = "block"
 	        paypal.style.display = "none";
 	        paymentMessage.style.display = "none"}
-	        
+
 		 else if(paymentSelection.value === "select method"){
 			bitcoin.style.display = "none";
 			creditCardDiv.style.display = "none"
@@ -150,7 +186,127 @@ paymentSelection.addEventListener("change", function(){
 	})
 
 
+/*******1234123412341234****** SUBMIT FORM SECTION **************/
 
+const button = document.getElementById("submit-button")
+const nameRegex = /^[a-z_ ]{3}/gi
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const creditCardRegex = /^[0-9]{16}/
+const zipcodeRegex = /^[0-9]{5}/
+const cvvRegex = /^[0-9]{3}/
+const nameAlertDiv = document.getElementById("nameAlert")
+const emailAlertDiv = document.getElementById("emailAlert")
+const alertName = document.createElement("p")
+const emailAlert = document.createElement("p")
+const creditAlert = document.createElement("p")
+const zipAlert = document.createElement("p")
+const cvvAlert = document.createElement("p")
+const workshopAlert = document.createElement("p")
+const creditCardPayment = document.querySelector(".col-6")
+const zipCodeAddress = document.querySelector(".col-3")
+const cvvDiv = document.getElementById("cvvDiv")
+const creditCardInput = document.getElementById("cc-num")
+const zipInput = document.getElementById("zip")
+const cvvNumber = document.getElementById("cvv")
+
+
+
+alertName.innerHTML = `Your name must: have at <strong>least 3 characters
+                       </strong> and must<strong> NOT</strong> contain any 
+                       numbers or symbols!`
+
+emailAlert.innerHTML = `Please enter a <strong>VALID</strong> email address`
+workshopAlert.innerHTML = `You must select at least <strong>ONE</strong> activity!`
+creditAlert.innerHTML = `Please enter a valid card number (16 digits required)`
+zipAlert.innerHTML = `Please enter a valid Zip Code (5 digits requred)`
+cvvAlert.innerHTML = `Please enter a valid CVV (3 digits required)`
+
+alertName.style.marginTop = "-4px"
+alertName.style.color = "red"
+emailAlert.style.marginTop = "-4px"
+emailAlert.style.color = "red"
+workshopAlert.style.color = "red"
+creditAlert.style.color = "red"
+zipAlert.style.color = "red"
+cvvAlert.style.color = "red"
+
+button.addEventListener("click", function(e){
+    let name = nameRegex.exec(nameInput.value)
+    let email = emailRegex.exec(emailInput.value)
+    let creditCardConfirm = creditCardRegex.exec(creditCardInput.value)
+    let zip = zipcodeRegex.exec(zipInput.value)
+    let cvvConfirm = cvvRegex.exec(cvvNumber.value)
+    
+		 if(!name){
+	         e.preventDefault()
+	         nameAlertDiv.appendChild(alertName)
+	         nameInput.focus() }
+		 else if (name){
+		 	 alertName.style.display = "none"
+		 	 nameInput.blur()
+		 }
+		
+		if(!email){
+			e.preventDefault()
+		    emailAlertDiv.appendChild(emailAlert)
+		    emailInput.focus() }
+	    else if (email){
+	     	 emailAlert.style.display = "none"
+	     	 emailInput.blur()
+	     }
+
+	     if(jobOptions.value === "other" && jobInfo.value === ''){
+	     	e.preventDefault()
+	     	extraJobInfoTag.innerHTML = "You must specify below"
+	     	extraJobInfoTag.style.color = "red"
+	     	extraJobInfoTag.focus()
+
+	     }
+
+	     if(totalCost === 0){
+	     	e.preventDefault()
+	     	workshops.appendChild(workshopAlert) }	     	
+	     else if(totalCost !== 0){
+	     	workshopAlert.style.display = "none"
+	     }
+	    
+	     if(paymentSelection.value === "select method"){
+	     	paymentMessage.style.color = "red"}	     
+	     else if(paymentSelection.vaue !== "select method"){
+	     	paymentMessage.style.display = "none"
+	     }
+        
+         if(!creditCardConfirm){
+         	e.preventDefault()
+         	creditCardInput.style.border = "3px solid red"
+            creditCardPayment.appendChild(creditAlert)}
+         else if(creditCardConfirm){
+         	creditCardInput.style.border = "none"
+         	creditAlert.style.display = "none"
+         }
+         if(!zip){
+         	e.preventDefault()
+         	zipInput.style.border = "3px solid red"
+         	zipCodeAddress.appendChild(zipAlert)}
+
+         else if(zip){
+         	zipInput.style.border = "none"
+         	zipAlert.style.display = "none"
+         }
+
+         if(!cvvConfirm){
+         	e.preventDefault()
+         	cvvNumber.style.border = "3px solid red"
+         	cvvDiv.appendChild(cvvAlert)
+         }
+         
+         else if(cvvConfirm){
+         	cvvNumber.style.border = "none"
+         	cvvDiv.style.display = "none"
+         }
+
+         
+	})
 
 
 
